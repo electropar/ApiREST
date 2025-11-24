06edec096803cffb12886800fbbdce4dcec92e5c
lgrossling <lgrossling@PC-022>
2025-11-24 08:56:14 -0300

    Corrección de problemas al recibir pusó en el nombre de la oportunidad

--- Webapi/Controllers/OportunidadController.cs
+++ Webapi/Controllers/OportunidadController.cs
@@ -19,7 +19,7 @@ namespace Webapi.Controllers
             string qu = "  if (select count(idoportunidad) from ELECTROPARAD.dbo.[OPN_ZOHO] where idoportunidad = '" + model.idOportunidad + "') > 0 " +
                         "  begin " +
                         "      update ELECTROPARAD.dbo.[OPN_ZOHO] " +
-                        "      set nombreoportunidad = upper('" + model.nombreOportunidad + "'), " +
+                        "      set nombreoportunidad = upper('" + model.nombreOportunidad.Replace("'", "") + "'), " +
                         "      nombrecliente = upper('" + model.nombreCliente + "'), " +
                         "      codigosapcliente = '" + model.codigoSAPCliente + "', " +
                         "      idpropietario = '" + model.idPropietario + "', " +
@@ -40,7 +40,7 @@ namespace Webapi.Controllers
                         "      declare @codigo integer set @codigo = isnull((select max(docentry) from ELECTROPARAD.dbo.[OPN_ZOHO] ),0) + 1 " +
                         "      insert into ELECTROPARAD.dbo.[OPN_ZOHO] (docentry, idoportunidad, nombreoportunidad, nombrecliente, codigosapcliente, idpropietario, propietario, fase, importeopn, monedaopn, fechacierre, descripcion, motivoperdio, motivoCancelo, fecharegistro, migrado) " +
                         "      values(@codigo, '" + model.idOportunidad + "', " +
-                        "      upper('" + model.nombreOportunidad + "'), " +
+                        "      upper('" + model.nombreOportunidad.Replace("'", "") + "'), " +
                         "      upper('" + model.nombreCliente + "' ) , " +
                         "      '" + model.codigoSAPCliente + "', " +
                         "      '" + model.idPropietario + "', " +
